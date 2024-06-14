6c5367a688de91816c253cfe12c3b98e304e25ca
Sarthak Chatterjee <sarthatc@gmail.com>
2024-06-14 23:43:22 +0530

    scroll to top

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import WebFont from "webfontloader";
 import Header from "./Component/Layout/Header/Header.js";
 import Footer from "./Component/Layout/Footer/Footer.js";
+import ScrollToTop from "./Component/Utils/ScrollToTop.js";
 import Home from "./Component/Home/Home.js";
 import NotFound from "./Component/Layout/Not Found/NotFound.js";
 import Experience from "./Component/Experience/Experience.js";
 import Projects from "./Component/Projects/Projects.js";
 import Blogs from "./Component/Blogs/Blogs.js";
 
-
 function App() {
-
   useEffect(() => {
     WebFont.load({
       google: {
@@ -23,6 +22,7 @@ function App() {
 
   return (
     <>
+      <ScrollToTop />
       <Header />
 
       <Routes>
